28f569ed3e2d14dbc6618c70910ad3aeced9e3a1
sprakashsingh <21097918+sprakashsingh@users.noreply.github.com>
2018-12-04 11:52:55 +0530

    Update StaticDemoApplication.java

--- src/main/java/com/example/demo/StaticDemoApplication.java
+++ src/main/java/com/example/demo/StaticDemoApplication.java
@@ -9,14 +9,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 @Controller
 public class StaticDemoApplication {
 	
 	@GetMapping("/")
-	public String home() {
-		return "forward:/static/index.html";
+	public ModelAndView home() {
+		return new ModelAndView(new RedirectView("https://demo.testfire.net"));
 	}
 
 	public static void main(String[] args) {
